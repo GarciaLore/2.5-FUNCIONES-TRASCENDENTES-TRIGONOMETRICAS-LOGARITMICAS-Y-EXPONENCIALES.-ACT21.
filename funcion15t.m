a9eba569816a6ec6c172cbad2e46e9a0d3a98c71
GarciaLore <91568849+GarciaLore@users.noreply.github.com>
2021-11-25 10:34:33 -0600

    FUNCION 15
    GRAFICACION DE LA FUNCION EN GNU OCTAVE Y DETERMINACION DE EL TIPO DE FUNCIONOCTAVE Y DETERMINACION DE EL TIPO DE FUNCION

---
+++ funcion15t.m
@@ -0,0 +1,42 @@
+% Octave Script 
+% Title             :TEMA 2.5 FUNCIONES TRSCENDENTES:TRIGONOMETRICAS, LOGARITMICAS Y EXPONENCIALES
+% Actividad         :21 F. Determinar,dominio,rango,tipo de funcion (Inyectiva,Suprayectiva y Biyectiva). Graficacion de las Funciones
+% School            :Colegio de Estudios Superiores de Jilotepec (TESJI)
+% Carer             :Ingenieria en Sistemas Computacionales (ISC)
+% Authors           :Garcia Garcia Lorena 
+% Date              :24/11/2021
+% Version           :1.0
+% Usage             :octave/path/
+% Notes             :Se requiere de la aplicacion de Octave para poder dar solucion a los problemas referentes a "LOS TIPOS DE FUNCIONES"
+% Funcion numero    :#15
+% Matricule         :202123044
+%                   :https://octaveintro.rradtrdees.ln/en/latrest/index.html
+%Borrar todo lo que se muestra en la hoja o comando windows (BTMHC)
+clc
+%Limpiamos variables (LV)
+clear
+%Importar el Paquete Symbolic (PS)
+pkg load symbolic 
+%Determinar el valor de la funcion a graficar (DVFG)
+ezplot ('tan(x)',[-20,20]);
+hold on;
+grid on;
+%Plotear la funcion a graficar (PFG)
+plot([-20,20],[0,0]);
+plot([0,0],[-8,8]);
+hold off;
+%Titulo de la funcion (TF)
+title("Funcion Sobreyectiva, Ejercicio15 f(x)=tan(x), con dominio (x|x?p/2pn) y rango (-oo,+oo)");
+%Valor del eje x (V.x)
+xlabel("x o VALOR DEL EJE X");
+%valor del eje y (V.y)
+ylabel("f(x)  o VALOR DEL EJE Y");
+%Dominio de la Funcion (DF)
+disp ('El dominio de la funcion es: (x|x?p/2pn)');
+%Rango de la Funcion (DF)
+disp ('El rango de la funcion es: (-oo,+oo)');
+%Tipo de la Funcion(TF)
+disp('Es sobreyectiva la funcion');
+disp ('xER (x pertenece a los numeros reales)');
+%Finalizacion del Codigo(FC)
+disp('El codigo y graficacion de la funcion pospuesta ha finalizado');
